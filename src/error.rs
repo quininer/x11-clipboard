af43d99c6bccc1ec116d315a79078d14239bf0ac
quininer <quininer@live.com>
2018-04-06 14:01:24 +0800

    clean Error type & add paste example

--- src/error.rs
+++ src/error.rs
@@ -3,12 +3,6 @@ pub type Result<T> = ::std::result::Result<T, Error>;
 #[derive(Debug, Fail)]
 #[must_use]
 pub enum Error {
-    #[fail(display = "io error: {:?}", _0)]
-    Io(::std::io::Error),
-
-    #[fail(display = "utf8 error: {:?}", _0)]
-    Utf8(::std::string::FromUtf8Error),
-
     #[fail(display = "set atom error: {:?}", _0)]
     Set(::std::sync::mpsc::SendError<::xcb::Atom>),
 
@@ -20,8 +14,10 @@ pub enum Error {
 
     #[fail(display = "store lock poison")]
     Lock,
+
     #[fail(display = "load selection timeout")]
     Timeout,
+
     #[fail(display = "set selection owner fail")]
     Owner
 }
@@ -36,8 +32,6 @@ macro_rules! define_from {
     }
 }
 
-define_from!(Io from ::std::io::Error);
-define_from!(Utf8 from ::std::string::FromUtf8Error);
 define_from!(Set from ::std::sync::mpsc::SendError<::xcb::Atom>);
 define_from!(XcbConn from ::xcb::base::ConnError);
 define_from!(XcbGeneric from ::xcb::base::GenericError);
--- src/lib.rs
+++ src/lib.rs
@@ -243,28 +243,3 @@ impl Clipboard {
         }
     }
 }
-
-
-#[test]
-fn it_work() {
-    let data = format!("{:?}", Instant::now());
-    let clipboard = Clipboard::new().unwrap();
-
-    let atom_clipboard = clipboard.setter.atoms.clipboard;
-    let atom_utf8string = clipboard.setter.atoms.utf8_string;
-    let atom_property = clipboard.setter.atoms.property;
-
-    clipboard.store(atom_clipboard, atom_utf8string, data.as_bytes()).unwrap();
-
-    let output = clipboard.load(atom_clipboard, atom_utf8string, atom_property, None).unwrap();
-    assert_eq!(output, data.as_bytes());
-
-    let data = format!("{:?}", Instant::now());
-    clipboard.store(atom_clipboard, atom_utf8string, data.as_bytes()).unwrap();
-
-    let output = clipboard.load(atom_clipboard, atom_utf8string, atom_property, None).unwrap();
-    assert_eq!(output, data.as_bytes());
-
-    let output = clipboard.load(atom_clipboard, atom_utf8string, atom_property, None).unwrap();
-    assert_eq!(output, data.as_bytes());
-}
